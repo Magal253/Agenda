cd592233a9aa1d0036c76b8fcc6234396a42e030
Magal253 <magallukas253@gmail.com>
2019-08-14 17:37:18 -0300

    fail_client_spec.test.js

--- spec/test/cliente_spec.test.js
+++ spec/test/cliente_spec.test.js
@@ -1,27 +1,58 @@
 var client = require('../../modelos/cliente');
 var Sequelize = require('sequelize');
 
+var connection1 = new Sequelize('user', 'root', 'masterkey',{
+    host:'localhost',
+    dialect: 'mysql'
+    });;
+
 it('nome esta vazio?', function(){
-    client.DataNasc ='2019/08/23';
-    client.Desc     ='nenhuma';
-    client.Email    ='oi@algum.coin';
-    client.Gastos   =123456;
-    client.InfoID   ='alguma info';
-    client.LinhaGes =35320000;
-    client.Metas    =10000000;
-    client.Nome     ='magal';
-    client.RazSoc   ='magal123';
-}).expect(client.nome).toBe('');
+
+  var cliente = connection1.define('cliente', {
+    DataNasc =client.DataNasc,
+    Desc     =client.Desc,
+    Email    =client.Email,
+    Gastos   =client.Gastos,
+    InfoID   =client.InfoID,
+    LinhaGes =client.LinhaGes,
+    Metas    =client.Metas,
+    Nome     =client.Nome,
+    RazSoc   =client.RazSoc      
+
+});
+    cliente.DataNasc ='2019/08/23';
+    cliente.Desc     ='nenhuma';
+    cliente.Email    ='oi@algum.coin';
+    cliente.Gastos   =123456;
+    cliente.InfoID   ='alguma info';
+    cliente.LinhaGes =35320000;
+    cliente.Metas    =10000000;
+    cliente.Nome     ='magal';
+    cliente.RazSoc   ='magal123';
+}).expect(cliente.nome).toBe('');
 
 
 it('o nome é magal?', function(){
-    client.DataNasc ='2019/08/23';
-    client.Desc     ='nenhuma';
-    client.Email    ='oi@algum.coin';
-    client.Gastos   =123456;
-    client.InfoID   ='alguma info';
-    client.LinhaGes =35320000;
-    client.Metas    =10000000;
-    client.Nome     ='magal';
-    client.RazSoc   ='magal123';
+ 
+    var cliente = connection1.define('cliente', {
+        DataNasc =client.DataNasc,
+        Desc     =client.Desc,
+        Email    =client.Email,
+        Gastos   =client.Gastos,
+        InfoID   =client.InfoID,
+        LinhaGes =client.LinhaGes,
+        Metas    =client.Metas,
+        Nome     =client.Nome,
+        RazSoc   =client.RazSoc      
+    
+    });
+        cliente.DataNasc ='2019/08/23';
+        cliente.Desc     ='nenhuma';
+        cliente.Email    ='oi@algum.coin';
+        cliente.Gastos   =123456;
+        cliente.InfoID   ='alguma info';
+        cliente.LinhaGes =35320000;
+        cliente.Metas    =10000000;
+        cliente.Nome     ='magal';
+        cliente.RazSoc   ='magal123';
 }).expect(client.nome).toBe('magal');
